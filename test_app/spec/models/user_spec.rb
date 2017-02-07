9b47b9b60fcbede9d00dc153a740917ab2ff07d2
Tana Jukes <tana.jukes@gmail.com>
2017-02-07 13:08:42 -0800

    Basic User spec (passing)
    The results of running this basic spec demonstrate that the pg sequencing issue (if that's what it is) that this test app was set up to investigate, is not present here.

--- test_app/spec/models/user_spec.rb
+++ test_app/spec/models/user_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
+# RSpec.describe User, type: :model do
+#   pending "add some examples to (or delete) #{__FILE__}"
+# end
+
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid by default" do
+    user = User.new
+    user.save!
+    p user
+    expect(user).to be_valid
+  end
 end
